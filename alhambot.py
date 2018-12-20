a3411d90e02c25f16f32176534cf42792910cc16
Jonathan Tan <jonathantan@cs.uchicago.edu>
2018-12-19 18:40:57 -0600

    relaxing constraints to test if github integration is working

--- alhambot.py
+++ alhambot.py
@@ -45,8 +45,7 @@ def matches_al_ham(text):
         if len(pattern) != 7:
             return False
         else:
-            on_stresses = all(int(pattern[0]) > 0, int(pattern[2]) > 0,
-                              int(pattern[4]) > 0)
+            on_stresses = all(int(pattern[0]) > 0, int(pattern[4]) > 0)
             off_stresses = all(int(pattern[1] == 0), int(pattern[3]) == 0,
                                int(pattern[5]) == 0)
             return on_stresses # and off_stresses
